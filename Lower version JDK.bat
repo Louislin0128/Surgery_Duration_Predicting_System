e114f2819ec9e023631e810b83151c83479aaa33
Louislin0128 <lin05191123@gmail.com>
2022-10-07 01:53:26 +0800

    redo

--- Lower version JDK.bat
+++
@@ -1,4 +0,0 @@
-javac -cp Jar\*; -d classes src\gui\*.java src\guiComponent\*.java src\guiFunction\*.java src\predict\*.java src\preprocess\*.java
-java -Xms64m -Xmx4096m -cp Jar\*;.\classes; gui.ProcessFrame
-
-pause
---
+++ Surgery_Time_Prediction_System/DataSplitPanel.bat
@@ -0,0 +1,4 @@
+PATH C:\Program Files\Java\jdk-17.0.2\bin
+javac -cp Jar\*; -d classes gui\*.java guiComponent\*.java guiFunction\*.java
+java -cp classes gui.MainFrame DataSplitPanel
+pause
